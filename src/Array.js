61de1e7fa6414af68be2b3b13fabbfe06eb60b1d
Dharmanshu31 <d.j.paghadal.com>
2023-07-31 17:26:54 +0530

    commits

--- src/Array.js
+++ src/Array.js
@@ -1,22 +1,26 @@
 import { Button } from "@material-ui/core";
 import React from "react";
+import { styles } from './Style';
 
 export const Array=()=>{
     const book =["Bhagvatgita","wings of fire","Ramayan"];
+    const classes=styles();
     return(
         book.map((iteam)=>{
             return(
-                <React.Fragment>
+                <h1 className={classes.wraaper}>
                     <h3>{iteam}</h3>
                     <Button 
                     variant="contained" 
                     color="primary" 
                     size="small"
+                    className="click-btn"
+                    classes={{}}
                     onClick={()=>{
                         alert("You itam added sucessfully")
                     }}>
                     Add to cart</Button>
-                </React.Fragment>
+                </h1>
             );
         })
     );
--- src/Style.js
+++ src/Style.js
@@ -1,7 +1,9 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-export const style =makeStyles((theme)=>({
+export const styles =makeStyles((theme)=>({
     wraaper:{
-        
+        "& .click-btn":{
+            backgroundColor: "black"
+        }
     }
-}))
+}));
